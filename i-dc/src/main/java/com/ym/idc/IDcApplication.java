d91cd6442657184f942cacd77eb14cba6888cff8
liuqingmiao <liuqingmiao@chebada.com>
2020-05-09 15:43:46 +0800

    搭建项目

---
+++ i-dc/src/main/java/com/ym/idc/IDcApplication.java
@@ -0,0 +1,13 @@
+package com.ym.idc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDcApplication.class, args);
+    }
+
+}
---
+++ i-dc/src/test/java/com/ym/idc/IDcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ym.idc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
