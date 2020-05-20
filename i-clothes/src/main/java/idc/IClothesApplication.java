64e9d9e056f12b1329a23287fae6a53d82dca5df
liuqingmiao <liuqingmiao@chebada.com>
2020-05-20 13:51:51 +0800

    项目骨架搭建

---
+++ i-clothes/src/main/java/idc/IClothesApplication.java
@@ -0,0 +1,13 @@
+package idc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IClothesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IClothesApplication.class, args);
+    }
+
+}
---
+++ i-clothes/src/main/java/idc/controller/SpecController.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright: 2016 www.chebada.com Inc. All rights reserved.
+ * 注意：本内容仅限于车巴达公司内部传阅，禁止外泄以及用于其他的商业目的
+ *
+ * @Project: pioneer
+ * @File: SpecController
+ * @Package: com.ym.idc.controller
+ * @Date: 2020年05月20日
+ * @Author:lqm48330@chebada.com
+ *
+ */
+package idc.controller;
+
+/**
+ * @description: specification
+ * @author: lqm48330@chebada.com
+ * @date: 2020年05月20日 11:08:28
+ **/
+public class SpecController {
+}
